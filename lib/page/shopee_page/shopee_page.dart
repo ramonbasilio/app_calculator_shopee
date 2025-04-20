b897dfdf40641f614feede9880a41b588f30f47a
ramonbasilio <ramon.basilio@hotmail.com>
2025-04-19 23:30:30 -0300

    commit

--- lib/page/shopee_page/shopee_page.dart
+++ lib/page/shopee_page/shopee_page.dart
@@ -21,167 +21,171 @@ class _ShopeePageState extends State<ShopeePage> {
     return Scaffold(
       backgroundColor: Color(0xFF17181C),
       resizeToAvoidBottomInset: true,
-
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Container(
-              width: 500,
-              height: MediaQuery.of(context).size.height - 190,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.white, width: 0.9),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Shopee',
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'Custo do Produto',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Color.fromARGB(255, 73, 80, 84),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'R\$ ',
-                        style: TextStyle(fontSize: 50, color: Colors.white),
-                      ),
-                      Flexible(
-                        child: TextFormField(
-                          inputFormatters: [
-                            FilteringTextInputFormatter.allow(
-                              RegExp(r'[0-9,]'),
-                            ),
-                          ],
-                          keyboardType: TextInputType.numberWithOptions(
-                            decimal: true,
-                          ),
-                          controller: _custController,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            isDense: true,
-                            contentPadding: EdgeInsets.zero,
-                            hintText: '0,00',
-                          ),
-                          style: TextStyle(fontSize: 50, color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'Lucro desejado',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Color.fromARGB(255, 73, 80, 84),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        '% ',
-                        style: TextStyle(fontSize: 50, color: Colors.white),
-                      ),
-                      Flexible(
-                        child: TextFormField(
-                          inputFormatters: [
-                            FilteringTextInputFormatter.allow(
-                              RegExp(r'[0-9,]'),
-                            ),
-                          ],
-                          keyboardType: TextInputType.numberWithOptions(
-                            decimal: true,
-                          ),
-                          controller: _gainController,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            isDense: true,
-                            contentPadding: EdgeInsets.zero,
-                            hintText: '00',
-                          ),
-                          style: TextStyle(fontSize: 50, color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        final calculus = Calculus();
-                        final resultValue = calculus.calculusShopee(
-                          _custController.text,
-                          _gainController.text,
-                          context,
-                        );
-
-                        if (resultValue != null) {
-                          setState(() {
-                            result = resultValue;
-                          });
-                        }
-                      },
-                      child: const Text(
-                        'Calcular',
-                        style: TextStyle(color: Color(0xFF17181C)),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    width: 500,
-                    height: 150,
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Container(
+                    width: double.infinity, // Ajusta à largura da tela
                     decoration: BoxDecoration(
-                      color: Color(0xFF332D2D),
-                      border: Border.all(color: Color(0xFF332D2D), width: 0.9),
+                      border: Border.all(color: Colors.white, width: 0.9),
                       borderRadius: BorderRadius.circular(10),
                     ),
                     padding: const EdgeInsets.all(20.0),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(
-                          'Preço do anúncio',
+                        const Text(
+                          'Shopee',
                           style: TextStyle(
-                            color: Color.fromARGB(255, 172, 176, 181),
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        FittedBox(
-                          fit: BoxFit.scaleDown,
-                          child: Text(
-                            NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$').format(result),
-                            style: TextStyle(fontSize: 60, color: Colors.white),
+                        const SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            'Custo do Produto',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromARGB(255, 73, 80, 84),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'R\$ ',
+                              style: TextStyle(fontSize: 50, color: Colors.white),
+                            ),
+                            Flexible(
+                              child: TextFormField(
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.allow(
+                                    RegExp(r'[0-9,]'),
+                                  ),
+                                ],
+                                keyboardType: TextInputType.numberWithOptions(
+                                  decimal: true,
+                                ),
+                                controller: _custController,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.zero,
+                                  hintText: '0,00',
+                                ),
+                                style: TextStyle(fontSize: 50, color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            'Lucro desejado',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromARGB(255, 73, 80, 84),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '% ',
+                              style: TextStyle(fontSize: 50, color: Colors.white),
+                            ),
+                            Flexible(
+                              child: TextFormField(
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.allow(
+                                    RegExp(r'[0-9,]'),
+                                  ),
+                                ],
+                                keyboardType: TextInputType.numberWithOptions(
+                                  decimal: true,
+                                ),
+                                controller: _gainController,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.zero,
+                                  hintText: '00',
+                                ),
+                                style: TextStyle(fontSize: 50, color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              final calculus = Calculus();
+                              final resultValue = calculus.calculusShopee(
+                                _custController.text,
+                                _gainController.text,
+                                context,
+                              );
+
+                              if (resultValue != null) {
+                                setState(() {
+                                  result = resultValue;
+                                });
+                              }
+                            },
+                            child: const Text(
+                              'Calcular',
+                              style: TextStyle(color: Color(0xFF17181C)),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          width: double.infinity,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF332D2D),
+                            border: Border.all(color: Color(0xFF332D2D), width: 0.9),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Preço do anúncio',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 172, 176, 181),
+                                ),
+                              ),
+                              FittedBox(
+                                fit: BoxFit.scaleDown,
+                                child: Text(
+                                  NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$').format(result),
+                                  style: TextStyle(fontSize: 60, color: Colors.white),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
                   ),
-                ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
