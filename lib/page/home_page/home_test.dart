5ac5d47000b53d69b2563dbe0906d97b7041d0da
ramonbasilio <ramon.basilio@hotmail.com>
2025-04-21 22:46:19 -0300

    commit 21042025

---
+++ lib/page/home_page/home_test.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_v1/page/amazon_page/amazon_page.dart';
+import 'package:flutter_application_v1/page/mercado_livre_page/mercado_livre_page.dart';
+import 'package:flutter_application_v1/page/shopee_page/shopee_page.dart';
+
+class HomePageTest extends StatefulWidget {
+  const HomePageTest({super.key});
+
+  @override
+  State<HomePageTest> createState() => _HomePageTestState();
+}
+
+class _HomePageTestState extends State<HomePageTest> {
+  int selectedIndex = 0;
+  static const List<Widget> widgetOptions = <Widget>[
+    ShopeePage(),
+    MercadoLivrePage(),
+    AmazonPage(),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isMobile = MediaQuery.of(context).size.width < 600;
+    return isMobile ? _buildMobileLayout() : _buildDesktopLayout();
+  }
+
+  Scaffold _buildMobileLayout() {
+    return Scaffold(
+      backgroundColor: Color(0xFF17181C),
+      appBar: AppBar(
+        title: const Text(
+          'Calculadora de Taxas - TESTE',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Color(0xFF332D2D), //0xFF17181C
+        centerTitle: true,
+      ),
+      body: Center(child: widgetOptions.elementAt(selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30), // Deixa redondo
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  5,
+                ), // Espaço interno para a imagem
+                child: Image.asset('lib/assets/shopee.png'),
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30), // Deixa redondo
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  5,
+                ), // Espaço interno para a imagem
+                child: Image.asset('lib/assets/mercado_livre.png'),
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30), // Deixa redondo
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  5,
+                ), // Espaço interno para a imagem
+                child: Image.asset('lib/assets/amazon.png'),
+              ),
+            ),
+            label: '',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Color(0xFF332D2D),
+        onTap: onItemTapped,
+      ),
+    );
+  }
+
+  Scaffold _buildDesktopLayout() {
+    return Scaffold(
+            appBar: AppBar(
+        title: const Text(
+          'Calculadora de Taxas - TESTE',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Color(0xFF332D2D), //0xFF17181C
+        centerTitle: true,
+      ),
+      body: Row(
+        children: [
+          NavigationRail(
+            backgroundColor: Color(0xFF332D2D),
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onItemTapped,
+            labelType: NavigationRailLabelType.none,
+            destinations: [
+              NavigationRailDestination(
+                icon: Container(
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30), // Deixa redondo
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Image.asset('lib/assets/shopee.png'),
+                  ),
+                ),
+                label: Text('Shopee'),
+              ),
+              NavigationRailDestination(
+                icon: Container(
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30), // Deixa redondo
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Image.asset('lib/assets/mercado_livre.png'),
+                  ),
+                ),
+                label: Text(''),
+              ),
+              NavigationRailDestination(
+                icon: Container(
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30), // Deixa redondo
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(
+                      5,
+                    ), // Espaço interno para a imagem
+                    child: Image.asset('lib/assets/amazon.png'),
+                  ),
+                ),
+                label: Text(''),
+              ),
+            ],
+          ),
+          VerticalDivider(thickness: 1, width: 1),
+          Expanded(child: widgetOptions[selectedIndex]),
+        ],
+      ),
+    );
+  }
+}
--- lib/page/shopee_page/shopee_page.dart
+++ lib/page/shopee_page/shopee_page.dart
@@ -18,6 +18,7 @@ class _ShopeePageState extends State<ShopeePage> {
 
   @override
   Widget build(BuildContext context) {
+   final isMobile = MediaQuery.of(context).size.width < 600;
     return Scaffold(
       backgroundColor: Color(0xFF17181C),
       resizeToAvoidBottomInset: true,
@@ -28,158 +29,160 @@ class _ShopeePageState extends State<ShopeePage> {
               child: SingleChildScrollView(
                 child: Padding(
                   padding: const EdgeInsets.all(10),
-                  child: Container(
-                    width: double.infinity, // Ajusta à largura da tela
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 0.9),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Shopee',
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: const Text(
-                            'Custo do Produto',
+                  child: Center(
+                    child: Container(
+                      width: isMobile ? double.infinity : 500, // Ajusta à largura da tela
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 0.9),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Shopee',
                             style: TextStyle(
-                              fontSize: 15,
-                              color: Color.fromARGB(255, 73, 80, 84),
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              'R\$ ',
-                              style: TextStyle(fontSize: 50, color: Colors.white),
-                            ),
-                            Flexible(
-                              child: TextFormField(
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.allow(
-                                    RegExp(r'[0-9,]'),
-                                  ),
-                                ],
-                                keyboardType: TextInputType.numberWithOptions(
-                                  decimal: true,
-                                ),
-                                controller: _custController,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  isDense: true,
-                                  contentPadding: EdgeInsets.zero,
-                                  hintText: '0,00',
-                                ),
-                                style: TextStyle(fontSize: 50, color: Colors.white),
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: const Text(
-                            'Lucro desejado',
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Color.fromARGB(255, 73, 80, 84),
-                            ),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              '% ',
-                              style: TextStyle(fontSize: 50, color: Colors.white),
-                            ),
-                            Flexible(
-                              child: TextFormField(
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.allow(
-                                    RegExp(r'[0-9,]'),
-                                  ),
-                                ],
-                                keyboardType: TextInputType.numberWithOptions(
-                                  decimal: true,
-                                ),
-                                controller: _gainController,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  isDense: true,
-                                  contentPadding: EdgeInsets.zero,
-                                  hintText: '00',
-                                ),
-                                style: TextStyle(fontSize: 50, color: Colors.white),
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              final calculus = Calculus();
-                              final resultValue = calculus.calculusShopee(
-                                _custController.text,
-                                _gainController.text,
-                                context,
-                              );
-
-                              if (resultValue != null) {
-                                setState(() {
-                                  result = resultValue;
-                                });
-                              }
-                            },
+                          const SizedBox(height: 10),
+                          Align(
+                            alignment: Alignment.centerLeft,
                             child: const Text(
-                              'Calcular',
-                              style: TextStyle(color: Color(0xFF17181C)),
+                              'Custo do Produto',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Color.fromARGB(255, 73, 80, 84),
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 10),
-                        Container(
-                          width: double.infinity,
-                          height: 150,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF332D2D),
-                            border: Border.all(color: Color(0xFF332D2D), width: 0.9),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          Row(
                             children: [
                               Text(
-                                'Preço do anúncio',
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 172, 176, 181),
-                                ),
+                                'R\$ ',
+                                style: TextStyle(fontSize: 50, color: Colors.white),
                               ),
-                              FittedBox(
-                                fit: BoxFit.scaleDown,
-                                child: Text(
-                                  NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$').format(result),
-                                  style: TextStyle(fontSize: 60, color: Colors.white),
+                              Flexible(
+                                child: TextFormField(
+                                  inputFormatters: [
+                                    FilteringTextInputFormatter.allow(
+                                      RegExp(r'[0-9,]'),
+                                    ),
+                                  ],
+                                  keyboardType: TextInputType.numberWithOptions(
+                                    decimal: true,
+                                  ),
+                                  controller: _custController,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    isDense: true,
+                                    contentPadding: EdgeInsets.zero,
+                                    hintText: '0,00',
+                                  ),
+                                  style: TextStyle(fontSize: 50, color: Colors.white),
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                      ],
+                          const SizedBox(height: 10),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: const Text(
+                              'Lucro desejado',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Color.fromARGB(255, 73, 80, 84),
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                '% ',
+                                style: TextStyle(fontSize: 50, color: Colors.white),
+                              ),
+                              Flexible(
+                                child: TextFormField(
+                                  inputFormatters: [
+                                    FilteringTextInputFormatter.allow(
+                                      RegExp(r'[0-9,]'),
+                                    ),
+                                  ],
+                                  keyboardType: TextInputType.numberWithOptions(
+                                    decimal: true,
+                                  ),
+                                  controller: _gainController,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    isDense: true,
+                                    contentPadding: EdgeInsets.zero,
+                                    hintText: '00',
+                                  ),
+                                  style: TextStyle(fontSize: 50, color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                final calculus = Calculus();
+                                final resultValue = calculus.calculusShopee(
+                                  _custController.text,
+                                  _gainController.text,
+                                  context,
+                                );
+                    
+                                if (resultValue != null) {
+                                  setState(() {
+                                    result = resultValue;
+                                  });
+                                }
+                              },
+                              child: const Text(
+                                'Calcular',
+                                style: TextStyle(color: Color(0xFF17181C)),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Container(
+                            width: double.infinity,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              color: Color(0xFF332D2D),
+                              border: Border.all(color: Color(0xFF332D2D), width: 0.9),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Preço do anúncio',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 172, 176, 181),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.scaleDown,
+                                  child: Text(
+                                    NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$').format(result),
+                                    style: TextStyle(fontSize: 60, color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
